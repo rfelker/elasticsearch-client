3664c83966bc1f95a8f60756a85d9763b239a1e1
Rolf Felker <rolf.felker@codecentric.de>
2015-12-21 16:18:01 +0100

    gui

--- src/main/java/de/codecentric/elasticsearch/client/verticle/MyFirstVerticle.java
+++ src/main/java/de/codecentric/elasticsearch/client/verticle/MyFirstVerticle.java
@@ -1,19 +1,54 @@
 package de.codecentric.elasticsearch.client.verticle;
 
+import de.codecentric.elasticsearch.model.Whisky;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
-
     public static final int FALLBACK_PORT = 8080;
 
+    // Store our product
+    private Map<Integer, Whisky> products = new LinkedHashMap<>();
+
     public void start(Future<Void> future) {
+
+        createSomeData();
+
+        // Create a router object.
+        Router router = Router.router(vertx);
+
+        // Bind "/" to our hello message - so we are still compatible.
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>Hello from my first Vert.x 3 application</h1>");
+        });
+
+        // Serve static resources from the /assets directory
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
+        router.get("/api/whiskies").handler(this::getAll);
+        router.route("/api/whiskies*").handler(BodyHandler.create());
+        router.post("/api/whiskies").handler(this::addOne);
+        router.delete("/api/whiskies/:id").handler(this::deleteOne);
+        router.get("/api/whiskies/:id").handler(this::getOne);
+        router.put("/api/whiskies/:id").handler(this::updateOne);
+
+
+        // Create the HTTP server and pass the "accept" method to the request handler
         vertx
                 .createHttpServer()
-                .requestHandler(r -> {
-                    r.response().end("<h1>Hello from my first Vert.x 3 application</h1>");
-                })
+                .requestHandler(router::accept)
                 .listen(config().getInteger("http.port", FALLBACK_PORT),
                         result -> {
                             if (result.succeeded()) {
@@ -22,6 +57,96 @@ public class MyFirstVerticle extends AbstractVerticle {
                                 future.fail(result.cause());
                             }
                         });
+
+    }
+
+    private void deleteOne(RoutingContext routingContext) {
+        String id = routingContext.request().getParam("id");
+
+        if (id == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            Integer idAsInteger = Integer.valueOf(id);
+            products.remove(idAsInteger);
+        }
+
+        routingContext.response().setStatusCode(204).end();
+    }
+
+    private void addOne(RoutingContext routingContext) {
+        final Whisky whisky = Json.decodeValue(routingContext.getBodyAsString(), Whisky.class);
+
+        products.put(whisky.getId(), whisky);
+        routingContext.response()
+                .setStatusCode(201)
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(whisky));
+
+    }
+
+    private void getOne(RoutingContext routingContext) {
+        String id = routingContext.request().getParam("id");
+
+        if (id == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            Integer idAsInteger = Integer.valueOf(id);
+            Whisky whisky = products.get(idAsInteger);
+
+            if(whisky == null){
+                routingContext.response().setStatusCode(404).end();
+            }
+            else {
+                routingContext.response().
+                        putHeader("content-type", "application/json; charset=utf-8")
+                        .end(Json.encodePrettily(whisky));
+            }
+        }
+    }
+
+    private void updateOne(RoutingContext routingContext) {
+        String id = routingContext.request().getParam("id");
+
+        if (id == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            Integer idAsInteger = Integer.valueOf(id);
+            Whisky whisky = products.get(idAsInteger);
+
+            if (whisky == null) {
+                routingContext.response().setStatusCode(404).end();
+            } else {
+                final Whisky whiskyUpdate = Json.decodeValue(routingContext.getBodyAsString(), Whisky.class);
+
+                if(whiskyUpdate == null){
+                    routingContext.response().setStatusCode(400).end();
+                }
+
+                whisky.setName(whiskyUpdate.getName());
+                whisky.setOrigin(whiskyUpdate.getOrigin());
+
+                routingContext.response()
+                        .setStatusCode(200)
+                        .putHeader("content-type", "application/json; charset=utf-8")
+                        .end(Json.encodePrettily(whisky));
+            }
+        }
+    }
+
+    private void getAll(RoutingContext routingContext) {
+        routingContext.response()
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(products.values()));
+    }
+
+
+    private void createSomeData() {
+
+        Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
+        products.put(bowmore.getId(), bowmore);
+
+        Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
+        products.put(talisker.getId(), talisker);
     }
 
 }
--- src/test/java/de/codecentric/elasticsearch/client/verticle/MyFirstVerticleTest.java
+++ src/test/java/de/codecentric/elasticsearch/client/verticle/MyFirstVerticleTest.java
@@ -1,7 +1,9 @@
 package de.codecentric.elasticsearch.client.verticle;
 
+import de.codecentric.elasticsearch.model.Whisky;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -49,6 +51,46 @@ public class MyFirstVerticleTest {
                     context.assertTrue(body.toString().contains("Hello"));
                     async.complete();
                 }));
+    }
+
+    @Test
+    public void checkThatTheIndexPageIsServed(TestContext context) {
+        final Async async = context.async();
+
+        vertx.createHttpClient().getNow(port, "localhost", "/assets/index.html",
+                response -> {
+                    context.assertEquals(response.statusCode(), 200);
+                    context.assertEquals(response.headers().get("content-type"), "text/html");
+                    response.bodyHandler(body -> {
+                        context.assertTrue(body.toString().contains("<title>My Whisky Collection</title>"));
+                        async.complete();
+                    });
+                });
+    }
+
+    @Test
+    public void checkThatWeCanAdd(TestContext context) {
+        Async async = context.async();
+        final String json = Json.encodePrettily(new Whisky("Jameson", "Ireland"));
+        final String length = Integer.toString(json.length());
+
+        vertx.createHttpClient()
+                .post(port, "localhost", "/api/whiskies")
+                .putHeader("content-type", "application/json")
+                .putHeader("content-length", length)
+                .handler(response -> {
+                    context.assertEquals(response.statusCode(), 201);
+                    context.assertTrue(response.headers().get("content-type").contains("application/json"));
+                    response.bodyHandler(body -> {
+                        final Whisky whisky = Json.decodeValue(body.toString(), Whisky.class);
+                        context.assertEquals(whisky.getName(), "Jameson");
+                        context.assertEquals(whisky.getOrigin(), "Ireland");
+                        context.assertNotNull(whisky.getId());
+                        async.complete();
+                    });
+                })
+                .write(json)
+                .end();
     }
 
 }
